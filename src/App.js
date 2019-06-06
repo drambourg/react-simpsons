d315722aeedaa2b6eb132d5bd759040e163cbad2
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-06-06 21:49:01 +0200

    Création du composant Quotes - tableau et map des images - Inscription du composant dans APp.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Quote from "./Quote";
+import Quotes from "./Quotes";
 
 
 class App extends Component {
@@ -23,6 +24,7 @@ class App extends Component {
                     character="Ralph Wiggum"
                     image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
                 />
+                <Quotes />
             </div>
         );
     }
