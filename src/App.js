7812c68565cecb1e60497714da8b18991aa13d22
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-06-07 08:31:07 +0200

    Msie en place d'une lampe et du State

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React , { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Quote from "./Quote";
 import Quotes from "./Quotes";
+import Lamp from "./Lamp";
 
 
 class App extends Component {
@@ -14,16 +14,12 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Simpsons Quotes</h1>
                 </header>
-                <Quote
-                    quote="I believe the children are the future... Unless we stop them now!"
-                    character="Homer Simpson"
-                    image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
-                />
-                <Quote
-                    quote="Me fail English? That's unpossible"
-                    character="Ralph Wiggum"
-                    image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
-                />
+                <hr/>
+                <h2>Lamp Exercise</h2>
+                <Lamp on />
+                <Lamp />
+                <hr/>
+                <h2>Quotes Exercise</h2>
                 <Quotes />
             </div>
         );
