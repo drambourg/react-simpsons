516e885f29ccc930d02e39f1263c650c994dcfbd
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-06-07 09:10:59 +0200

    Mise en place d'un state sur App

--- src/App.js
+++ src/App.js
@@ -7,13 +7,29 @@ import Lamp from "./Lamp";
 
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            working: props.working
+        };
+    }
+
+    handleClick = () => {
+        this.setState({ working: !this.state.working });
+    };
+
     render() {
+        const working = this.state.working ? 'working' : 'sleep';
         return (
             <div className="App">
                 <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
+                    <div   className="App-logo">
+                    <img src={logo}  className={working} alt="logo" />
+                    </div>
                     <h1 className="App-title">Simpsons Quotes</h1>
+                    <button onClick={this.handleClick} className={working}>{working.toUpperCase()}</button>
                 </header>
+
                 <hr/>
                 <h2>Lamp Exercise</h2>
                 <Lamp on />
